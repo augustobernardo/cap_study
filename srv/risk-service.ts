d1ece74da01e0b8fe0098344c2947b9f2a4a2c94
Augusto Oliveira <gutooliveira027@gmail.com>
2024-11-05 10:54:54 -0300

    ✨ adding hande to the service and criticality

---
+++ srv/risk-service.ts
@@ -0,0 +1,19 @@
+import cds from '@sap/cds';
+import { Risk } from "#cds-models/RiskService";
+import { Service } from "@sap/cds";
+
+// Criando um Handle para o serviço
+module.exports = cds.service.impl(async function (this: Service) {
+
+    this.after('READ', 'Risks', (risksData: Risk[]) => {
+        const risks = Array.isArray(risksData) ? risksData : [risksData];
+
+        risks.forEach(risk => {
+            if (risk.impact && risk.impact >= 100000) {
+                risk.criticality = 1;
+            } else {
+                risk.criticality = 2;
+            }
+        });
+    })
+})
